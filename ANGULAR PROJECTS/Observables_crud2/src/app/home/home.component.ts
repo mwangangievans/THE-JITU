48cb3a738e3ed8b72bb32fca7b2575d23e37432c
mwangangievans <muindemwangangi2@gmail.com>
2022-08-29 16:08:40 +0300

    obsevables

--- ANGULAR PROJECTS/Observables_crud2/src/app/home/home.component.ts
+++ ANGULAR PROJECTS/Observables_crud2/src/app/home/home.component.ts
@@ -1,30 +1,54 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { count } from 'console';
-import { interval, Observable, Subscription } from 'rxjs';
+
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit,OnDestroy {
-  private fisrtObsSubscription : Subscription
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
+
+  constructor() {
+  }
 
   ngOnInit() {
-    const customIntervalObservable =  new Observable(observer=>{
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
-      observer.next(count)
-      count++
-    }, 100);
+        observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
+        count++;
+      }, 1000);
     });
-    this.fisrtObsSubscription= customIntervalObservable.subscribe(data=>{
+
+
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
-      
-    })
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
+    });
   }
+
   ngOnDestroy(): void {
-    this.fisrtObsSubscription.unsubscribe();
+    this.firstObsSubscription.unsubscribe();
   }
+
 }
