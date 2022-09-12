3933d917a9af2a3c7f706a8eb6822ec66d64c173
mwangangievans <muindemwangangi2@gmail.com>
2022-09-12 23:22:27 +0300

    counter ngrx

--- ngrx_counter/src/app/counter/counter-button/counter-button.component.ts
+++ ngrx_counter/src/app/counter/counter-button/counter-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-counter-button',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterButtonComponent implements OnInit {
 
+  @Output() increment = new EventEmitter<void>()
+  @Output() decrement = new EventEmitter<void>()
+  @Output() reset = new EventEmitter<void>()
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  OnIncrement(){
+    this.increment.emit();
+  }
+
+  OnReset(){
+    this.reset.emit()
+  }
+
+  OnDecrement(){
+     this.decrement.emit()
+  }
 }
